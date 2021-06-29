25fb608bf1352a27983ffa5c2afb66870bcc4bc7
wyrkens <dawid.wyroslak@yahoo.com>
2021-06-29 16:05:16 +0200

    add Rollerblades controller

--- src/main/java/com/rollerbladeRentApp/service/mapper/CustomerMapper.java
+++ src/main/java/com/rollerbladeRentApp/service/mapper/CustomerMapper.java
@@ -3,7 +3,6 @@ package com.rollerbladeRentApp.service.mapper;
 import com.rollerbladeRentApp.api.model.Customer;
 import com.rollerbladeRentApp.api.model.NewCustomer;
 import com.rollerbladeRentApp.repository.customer.CustomerEntity;
-import org.springframework.context.annotation.Bean;
 
 public class CustomerMapper {
 
